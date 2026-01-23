d674deeebc2676bf07f174d9562917b968c2e44b
Ashraf-Khaled-2005 <ashrafkhaled200514@gmail.com>
2026-01-23 16:05:57 +0200

    solve comments

--- frontend/src/repositories/index.js
+++ frontend/src/repositories/index.js
@@ -1,7 +1,6 @@
 /**
  * Repositories Index - Central export for all repositories
  */
-export { default as userRepository } from './UserRepository';
 
 // Add more repository exports here as you create them
 // export { default as transcriptionRepository } from './TranscriptionRepository';
